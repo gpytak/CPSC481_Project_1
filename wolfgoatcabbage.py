d23b3c3740d7665fdfd31635c7ddf63640c09d3c
Gregory <85207059+gpytak@users.noreply.github.com>
2023-03-04 14:18:25 -0800

    Update wolfgoatcabbage.py

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -3,12 +3,15 @@ from search import *
 
 class WolfGoatCabbage(Problem):
 
-    def __init__(self, initial={'F','W','G','C'}, goal={}):
+    def __init__(self, initial={('F', 'W', 'G', 'C')}, goal=(frozenset())):
         super().__init__(initial, goal)
     
     def goal_test(self, state):
         """ Given a state, return True if state is a goal state, or False otherwise """
-        
+        # print()
+        # print("Goal: ", self.goal)
+        # print("State: ", state)
+        # print()
         return state == self.goal
 
     def result(self, state, action):
@@ -18,41 +21,45 @@ class WolfGoatCabbage(Problem):
         """ Given state and action, return a new state that is the result of the action.
         Action is assumed to be a valid action in the state """
 
-        new_state = list(state)
-        left_bank = []
+        new_state = state
+        left_bank = ()
+
+        print("State: ", tuple(state))
+        print("Action: ", list(action))
 
         # Moving Farmer and Goat from Left Bank
-        if state == {'F', 'W', 'G', 'C'}:  # {'F', 'G'}
-            left_bank = ['W', 'C']
-        if state == {'W', 'C'} and action == ['F']:   # {'F'}
-            left_bank = ['W', 'C']
+        if tuple(state) == ('F', 'W', 'G', 'C') and list(action) == ['F', 'G']:  # {'F', 'G')
+            left_bank = ('W', 'C')
+        if tuple(state) == ('W', 'C') or tuple(state) == ('C', 'W') and list(action) == ['F']:   # ('F')
+            left_bank = ('W', 'C')
         
-        # Taking Wolf or Cabbage from Left bank  {'F', 'W'}, or {'F', 'C'}
-        if state == {'W', 'C'} and action == ['F', 'W']: 
-            left_bank = ['C']
-        if state == {'W', 'C'} and action == ['F', 'C']:
-            left_bank = ['W']
+        # Taking Wolf or Cabbage from Left bank  ('F', 'W'), or ('F', 'C')
+        if tuple(state) == ('W', 'C') or tuple(state) == ('C', 'W') and list(action) == ['F', 'W']: 
+            left_bank = ('C',)
+        if tuple(state) == ('W', 'C') or tuple(state) == ('C', 'W') and list(action) == ['F', 'C']:
+            left_bank = ('W',)
 
-        # Taking Wolf from Left bank  # {'F', 'G'} or {'F', 'W'}
-        if state == {'W'} and action == ['F', 'G']:
-            left_bank = ['W']
-        if state == {'W'} and action == ['F', 'W']:
-            left_bank = ['G']
+        # Taking Wolf from Left bank  # ('F', 'G') or ('F', 'W')
+        if tuple(state) == ('W',) and list(action) == ['F', 'G']:
+            left_bank = ('W',)
+        if tuple(state) == ('W',) and list(action) == ['F', 'W']:
+            left_bank = ('G',)
 
-        # Taking Cabbage from Left bank  # {'F', 'G'} or {'F', 'C'}
-        if state == {'C'} and action == ['F', 'G']:
-            left_bank = ['C']
-        if state == {'C'} and action == ['F', 'C']:
-            left_bank = ['G']
+        # Taking Cabbage from Left bank  # ('F', 'G') or ('F', 'C')
+        if tuple(state) == ('C',) and list(action) == ['F', 'G']:
+            left_bank = ('C',)
+        if tuple(state) == ('C',) and list(action) == ['F', 'C']:
+            left_bank = ('G',)
 
-        # Taking Goat from Left bank # {'F'} or {'F', 'G'}
-        if state == {'G'} and action == ['F']:
-            left_bank = ['G']
-        if state == {'G'} and action == ['F', 'G']:
-            left_bank = []
+        # Taking Goat from Left bank # ('F') or ('F', 'G')
+        if tuple(state) == ('G',) and list(action) == ['F']:
+            left_bank = ('G',)
+        if tuple(state) == ('G',) and list(action) == ['F', 'G']:
+            left_bank = ()
 
+        print("left_bank: ", left_bank)
+        print()
         new_state = frozenset(left_bank)
-
         return new_state
 
 # State = Left Bank
@@ -65,21 +72,30 @@ class WolfGoatCabbage(Problem):
 
         possible_actions = [['F'], ['F', 'W'], ['F', 'G'], ['F', 'C']]
 
-        if state == {'F', 'W', 'G', 'C'}: # {'F', 'G'}
+        if tuple(state) == ('F', 'W', 'G', 'C'): # {'F', 'G'}
             del possible_actions[3]
             del possible_actions[1]
             del possible_actions[0]
-        if state == {'W', 'C'}:  # {'F'}, {'F', 'W'}, or {'F', 'C'}
+        if tuple(state) == ('W', 'C') or tuple(state) == ('C', 'W'):  # {'F'}, {'F', 'W'}, or {'F', 'C'}
             del possible_actions[2]
-        if state == {'W'}: # {'F', 'G'} or {'F', 'W'}
+        if tuple(state) == ('W',): # {'F', 'G'} or {'F', 'W'}
             del possible_actions[3]
             del possible_actions[0]
-        if state == {'C'}: # {'F', 'G'} or {'F', 'C'}
+        if tuple(state) == ('C',): # {'F', 'G'} or {'F', 'C'}
             del possible_actions[1]
             del possible_actions[0]
-        if state == {'G'}: # {'F'} or {'F', 'G'}
+        if tuple(state) == ('G',): # {'F'} or {'F', 'G'}
             del possible_actions[3]
             del possible_actions[1]
+        if tuple(state) == ():
+            del possible_actions[3]
+            del possible_actions[2]
+            del possible_actions[1]
+            del possible_actions[0]
+
+        print("State: ", tuple(state))
+        print("possible_actions: ", possible_actions)
+        print()
 
         return possible_actions
 
