87c0ce32f3b764e665d8dea779ed3f19fb2cc5d6
Gregory <85207059+gpytak@users.noreply.github.com>
2023-03-04 18:21:56 -0800

    Update wolfgoatcabbage.py

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -6,7 +6,7 @@ from search import *
 
 class WolfGoatCabbage(Problem):
 
-    def __init__(self, initial=(("F", "W", "G", "C")), goal=(frozenset())):
+    def __init__(self, initial=(frozenset(("F", "W", "G", "C"))), goal=(frozenset())):
         super().__init__(initial, goal)
     
     def goal_test(self, state):
@@ -21,33 +21,33 @@ class WolfGoatCabbage(Problem):
         new_state = {}
 
         # Moving Farmer and Goat from Left Bank
-        if set(state) == {"F", "W", "G", "C"} and action == {"F", "G"}:  # {"F", "G")
+        if frozenset(frozenset(set(state))) == frozenset({"F", "W", "G", "C"}) and action == {"F", "G"}:  # ("F", "G")
             new_state = {"W", "C"}
         
         # Taking Wolf or Cabbage from Left bank  # ("F"), ("F", "W"), or ("F", "C")
-        if set(state) == {"W", "C"} and action == {"F"}:
-            new_state = {"F", "W", "C"}
-        if set(state) == {"F", "W", "C"} and action == {"F", "W"}:
-            new_state = ("C",)
-        if set(state) == {"F", "W", "C"} and action == {"F", "C"}:
+        if frozenset(set(state)) == frozenset({"W", "C"}) and action == {"F"}:
+            new_state = frozenset({"F", "W", "C"})
+        if frozenset(set(state)) == frozenset({"F", "W", "C"}) and action == frozenset({"F", "W"}):
+            new_state = {"C",}
+        if frozenset(set(state)) == frozenset({"F", "W", "C"}) and action == frozenset({"F", "C"}):
             new_state = {"W",}
 
         # Taking Wolf from Left bank  # ("F", "G") or ("F", "W")
-        if set(state) == {"W",} and action == {"F", "G"}:
+        if frozenset(set(state)) == frozenset({"W",}) and action == {"F", "G"}:
             new_state = {"F", "W", "G"}
-        if set(state) == {"F", "W", "G"} and action == {"F", "W"}:
+        if frozenset(set(state)) == frozenset({"F", "W", "G"}) and action == {"F", "W"}:
             new_state = {"G",}
 
         # Taking Cabbage from Left bank  # ("F", "G") or ("F", "C")
-        if set(state) == {"C",} and action == {"F", "G"}:
+        if frozenset(set(state)) == frozenset({"C",}) and action == {"F", "G"}:
             new_state = {"F", "G", "C"}
-        if set(state) == {"F", "G", "C"} and action == {"F", "C"}:
+        if frozenset(set(state)) == frozenset({"F", "G", "C"}) and action == {"F", "C"}:
             new_state = {"G",}
 
         # Taking Goat from Left bank # ("F") or ("F", "G")
-        if set(state) == {"G",} and action == {"F"}:
+        if frozenset(set(state)) == frozenset({"G",}) and action == {"F"}:
             new_state = {"F", "G"}
-        if set(state) == {"F", "G"} and action == {"F", "G"}:
+        if frozenset(set(state)) == frozenset({"F", "G"}) and action == {"F", "G"}:
             new_state = {}
         
         return frozenset(new_state)
@@ -59,42 +59,42 @@ class WolfGoatCabbage(Problem):
 
         possible_actions = [{"F"}, {"F", "W"}, {"F", "G"}, {"F", "C"}]
 
-        if set(state) == {"F", "W", "G", "C"}: # {"F", "G"}
+        if frozenset(set(state)) == {"F", "W", "G", "C"}: # {"F", "G"}
             del possible_actions[3]
             del possible_actions[1]
             del possible_actions[0]
-        if set(state) == {"W", "C"}:  # {"F"}
-            del possible_actions[3]
-            del possible_actions[2]
-            del possible_actions[1]
-        if set(state) == {"F", "W", "C"}: # {"F", "W"} or {"F", "C"}
-            del possible_actions[2]
-            del possible_actions[0]
-        if set(state) == {"W",}: # {"F", "G"}
-            del possible_actions[3]
-            del possible_actions[1]
-            del possible_actions[0]
-        if set(state) == {"F", "W", "G"}: # {"F", "W"}
-            del possible_actions[3]
-            del possible_actions[2]
-            del possible_actions[0]
-        if set(state) == {"C",}: # {"F", "G"}
-            del possible_actions[3]
-            del possible_actions[1]
-            del possible_actions[0]
-        if set(state) == {"F", "G", "C"}: # {"F", "C"}
-            del possible_actions[2]
-            del possible_actions[1]
-            del possible_actions[0]
-        if set(state) == {"G",}: # {"F"}
+        if frozenset(set(state)) == {"W", "C"}:  # {"F"}
             del possible_actions[3]
             del possible_actions[2]
             del possible_actions[1]
-        if set(state) == {"F", "G"}: # {"F", "G"}
+        if frozenset(set(state)) == frozenset({"F", "W", "C"}): # {"F", "W"} or {"F", "C"}
+            del possible_actions[2]
+            del possible_actions[0]
+        if frozenset(set(state)) == frozenset({"W",}): # {"F", "G"}
             del possible_actions[3]
             del possible_actions[1]
             del possible_actions[0]
-        if set(state) == {}:
+        if frozenset(set(state)) == {"F", "W", "G"}: # {"F", "W"}
+            del possible_actions[3]
+            del possible_actions[2]
+            del possible_actions[0]
+        if frozenset(set(state)) == {"C",}: # {"F", "G"}
+            del possible_actions[3]
+            del possible_actions[1]
+            del possible_actions[0]
+        if frozenset(set(state)) == {"F", "G", "C"}: # {"F", "C"}
+            del possible_actions[2]
+            del possible_actions[1]
+            del possible_actions[0]
+        if frozenset(set(state)) == {"G",}: # {"F"}
+            del possible_actions[3]
+            del possible_actions[2]
+            del possible_actions[1]
+        if frozenset(set(state)) == {"F", "G"}: # {"F", "G"}
+            del possible_actions[3]
+            del possible_actions[1]
+            del possible_actions[0]
+        if frozenset(set(state)) == {}:
             del possible_actions[3]
             del possible_actions[2]
             del possible_actions[1]
@@ -103,7 +103,7 @@ class WolfGoatCabbage(Problem):
         return possible_actions
 
 def main():
-    wgc = WolfGoatCabbage(("F", "W", "G", "C"))
+    wgc = WolfGoatCabbage(frozenset(("F", "W", "G", "C")))
     solution = depth_first_graph_search(wgc).solution()
     print(solution)
     solution = breadth_first_graph_search(wgc).solution()
